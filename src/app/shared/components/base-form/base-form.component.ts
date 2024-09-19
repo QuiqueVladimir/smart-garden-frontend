5009034f91f28cb25e01ea7b039b0222e52b62a1
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-19 09:50:23 -0500

    feat: initial commit

---
+++ src/app/shared/components/base-form/base-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './base-form.component.html',
+  styleUrl: './base-form.component.css'
+})
+export class BaseFormComponent {
+
+}
