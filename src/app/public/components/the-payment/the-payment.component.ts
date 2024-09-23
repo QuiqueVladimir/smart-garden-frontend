b72196862ce89bef4c2c26ed8d9c9c6e4031a4d1
Alexander Justo <102604349+AlexanderJusto@users.noreply.github.com>
2024-09-23 17:06:49 -0500

    Add files via upload

---
+++ src/app/public/components/the-payment/the-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-the-payment',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './the-payment.component.html',
+  styleUrl: './the-payment.component.css'
+})
+export class ThePaymentComponent {
+
+}
