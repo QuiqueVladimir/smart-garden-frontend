7af313d6586eb7ea808719aafa94f7bc64c548b2
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-25 08:07:21 -0500

    feat(navigation): add side var component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TranslateService} from "@ngx-translate/core";
+import {SideNavigationBarComponent} from "./public/components/side-navigation-bar/side-navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
