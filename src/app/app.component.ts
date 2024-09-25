58d3c5adba85b9e084c2c6f16f140ff20ffc3ce4
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-25 18:40:25 -0500

    feat(routing): add login first page which to see user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LoginComponent} from "./public/pages/login/login.component";
 import {SideNavigationBarComponent} from "./public/components/side-navigation-bar/side-navigation-bar.component";
+import {NgIf} from "@angular/common";
+import {UserService} from "./shared/services/user.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent,SideNavigationBarComponent],
+  imports: [RouterOutlet, SideNavigationBarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'smart-garden-spa';
-  constructor() {
+  constructor(private userService: UserService) {}
 
+  isLoggedIn(): boolean{
+    return this.userService.isLoggedIn();
   }
 }
